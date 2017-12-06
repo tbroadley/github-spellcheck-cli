3bbb789fb73f33c326dc9085742073004becea39
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-06 01:19:50 -0500

    Spell-check all Markdown and text files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Git = require('nodegit');
 const tmp = require('tmp-promise');
 
@@ -14,12 +15,25 @@ tmp.dir({ unsafeCleanup: true })
   }).then(repo => {
     console.log('Getting the last commit from the master branch...');
     return repo.getMasterCommit();
-  }).then(master => {
-    console.log('Getting README.md from the master branch...');
-    return master.getEntry('README.md');
-  }).then(entry => entry.getBlob())
-  .then(blob => {
-    console.log('Getting misspelled words in README.md...');
-    return getMisspellings(blob.toString());
+  }).then(masterCommit => {
+    console.log('Getting the state of the working tree...');
+    return masterCommit.getTree();
+  }).then(tree => {
+    console.log('Getting a list of files in the working tree...');
+    return new Promise((resolve, reject) => {
+      const walker = tree.walk(true);
+      walker.on('end', resolve);
+      walker.on('error', reject);
+      walker.start();
+    });
+  }).then(treeEntries => {
+    console.log('Filtering the list of files to only include Markdown and text files...');
+    return _.filter(treeEntries, treeEntry => /\.(md|txt)$/.test(treeEntry.path()))
+  }).then(matchedTreeEntries => {
+    console.log('Spell-checking the remaining files...');
+    return Promise.all(_.map(matchedTreeEntries, entry => {
+      return entry.getBlob()
+        .then(blob => getMisspellings(blob.toString()));
+    }));
   }).then(result => console.log(result))
   .catch(error => console.error(error));
