6708480b885a45c8644b75df710b9f9610d28cf8
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-06 00:01:24 -0500

    Ignore misspellings in Markdown code

--- test/spellcheck_test.js
+++ test/spellcheck_test.js
@@ -13,6 +13,16 @@ describe('getMisspellings', () => {
   });
   after(() => mockery.disable());
 
+  function buildIndicesFromWords(document, words) {
+    return _.map(words, word => {
+      const start = document.indexOf(word);
+      return {
+        start,
+        end: start + word.length,
+      };
+    });
+  }
+
   function mockSpellchecker(indices, getCorrectionsForMisspelling) {
     mockery.deregisterMock('spellchecker');
     mockery.resetCache();
@@ -29,15 +39,62 @@ describe('getMisspellings', () => {
   });
 
   it('should return a single misspelling given a sentence with one misspelling', () => {
-    const indices = [{ start: 5, end: 12 }];
+    const document = 'Test sentenc';
+    const misspellings = ['sentenc'];
+    const indices = buildIndicesFromWords(document, misspellings);
     const suggestions = ['sentence', 'sententious'];
     const { getMisspellings } = mockSpellchecker(indices, _.constant(suggestions));
-    return getMisspellings('Test sentenc').should.eventually.deep.equal([
+    return getMisspellings(document).should.eventually.deep.equal([
       {
         index: _.first(indices),
         misspelling: 'sentenc',
         suggestions,
       },
     ]);
+  });
+
+  it('should handle Markdown backticks correctly', () => {
+    const document = '`libssl`. A rael spelling mistake. This `libary` is in a sentenc. `tset`';
+    const misspellings = ['libssl', 'rael', 'libary', 'sentenc', 'tset'];
+    const indices = buildIndicesFromWords(document, misspellings);
+    const suggestions = [];
+    const { getMisspellings } = mockSpellchecker(indices, _.constant(suggestions));
+    return getMisspellings(document).should.eventually.deep.equal(
+      _.map([indices[1], indices[3]], index => ({
+        index,
+        misspelling: document.substring(index.start, index.end),
+        suggestions: [],
+      }))
+    );
+  });
+
+  it('should handle Markdown triple backticks correctly', () => {
+    const document = `
+\`\`\`
+Code bolck
+\`\`\`
+Outside code bloock
+\`\`\`test
+Codee block with language
+\`\`\`
+
+Outisde code block again
+
+
+\`\`\` test2
+Code boock with language and space
+\`\`\`
+    `;
+    const misspellings = ['bolck', 'bloock', 'Codee', 'Outisde', 'boock'];
+    const indices = buildIndicesFromWords(document, misspellings);
+    const suggestions = [];
+    const { getMisspellings } = mockSpellchecker(indices, _.constant(suggestions));
+    return getMisspellings(document).should.eventually.deep.equal(
+      _.map([indices[1], indices[3]], index => ({
+        index,
+        misspelling: document.substring(index.start, index.end),
+        suggestions: [],
+      }))
+    );
   });
 })
