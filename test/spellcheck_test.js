463b5893e9708841753f9e988e01e3c478c06978
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-05 21:48:42 -0500

    Add a test for a misspelling

--- test/spellcheck_test.js
+++ test/spellcheck_test.js
@@ -6,23 +6,38 @@ chai.should();
 const _ = require('lodash');
 const mockery = require('mockery');
 
-function mockSpellchecker(checkSpelling, getCorrectionsForMisspelling) {
-  mockery.registerMock('spellchecker', {
-    checkSpellingAsync: () => Promise.resolve(checkSpelling()),
-    getCorrectionsForMisspelling,
-  });
-  return require('../lib/spellcheck');
-}
-
 describe('getMisspellings', () => {
   before(() => {
     mockery.registerAllowables(['fs', 'lodash', '../lib/spellcheck']);
-    mockery.enable();
+    mockery.enable({ useCleanCache: true });
   });
   after(() => mockery.disable());
+
+  function mockSpellchecker(checkSpelling, getCorrectionsForMisspelling) {
+    mockery.deregisterMock('spellchecker');
+    mockery.resetCache();
+    mockery.registerMock('spellchecker', {
+      checkSpellingAsync: () => Promise.resolve(checkSpelling()),
+      getCorrectionsForMisspelling,
+    });
+    return require('../lib/spellcheck');
+  }
 
   it('should return an empty array given a sentence with no misspellings', () => {
     const { getMisspellings } = mockSpellchecker(_.constant([]), _.noop);
     return getMisspellings('Test sentence').should.eventually.deep.equal([]);
   });
+
+  it('should a single misspelling given a sentence with one misspelling', () => {
+    const indices = [{ start: 5, end: 12 }];
+    const suggestions = ['sentence', 'sententious'];
+    const { getMisspellings } = mockSpellchecker(_.constant(indices), _.constant(suggestions));
+    return getMisspellings('Test sentenc').should.eventually.deep.equal([
+      {
+        index: _.first(indices),
+        misspelling: 'sentenc',
+        suggestions,
+      },
+    ]);
+  })
 })
