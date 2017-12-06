127d0e0ca8a37c0b29c1f6d2a5e5e43dd86ebeed
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-05 21:39:31 -0500

    Mock spellchecker with mockery

--- test/spellcheck_test.js
+++ test/spellcheck_test.js
@@ -3,11 +3,26 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 chai.should();
 
-const { getMisspellings } = require('../lib/spellcheck');
+const _ = require('lodash');
+const mockery = require('mockery');
+
+function mockSpellchecker(checkSpelling, getCorrectionsForMisspelling) {
+  mockery.registerMock('spellchecker', {
+    checkSpellingAsync: () => Promise.resolve(checkSpelling()),
+    getCorrectionsForMisspelling,
+  });
+  return require('../lib/spellcheck');
+}
 
 describe('getMisspellings', () => {
+  before(() => {
+    mockery.registerAllowables(['fs', 'lodash', '../lib/spellcheck']);
+    mockery.enable();
+  });
+  after(() => mockery.disable());
+
   it('should return an empty array given a sentence with no misspellings', () => {
-    const document = 'The quick brown fox jumps over the lazy dog';
-    return getMisspellings(document).should.eventually.deep.equal([]);
+    const { getMisspellings } = mockSpellchecker(_.constant([]), _.noop);
+    return getMisspellings('Test sentence').should.eventually.deep.equal([]);
   });
 })
