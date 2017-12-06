fe9b9e1f919ff6e316885181bfefc85d58f4fae4
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-05 21:11:33 -0500

    Move spellchecking code into a library

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const _ = require('lodash');
-const SpellChecker = require('spellchecker');
+const { getMisspellings } = require('./lib/spellcheck');
 
 const document = 'The quikc (brown) foxx jumps ovr teh lazy dgo...';
-SpellChecker.checkSpellingAsync(document)
-  .then(indices => {
-    const misspellings = indices.map(({ start, end }) => document.substring(start, end));
-    const suggestions = misspellings.map(SpellChecker.getCorrectionsForMisspelling);
-    return _.zipWith(indices, misspellings, suggestions, (index, misspelling, suggestions) => ({
-      index,
-      misspelling,
-      suggestions,
-    }));
-  })
+getMisspellings(document)
   .then(result => {
     console.log(JSON.stringify(result, null, 2));
   });
