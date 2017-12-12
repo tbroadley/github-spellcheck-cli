4de4c6794ec580203e2dc8bc872557f142eebd43
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-11 21:44:37 -0500

    Fix issue with adding changes

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ async function go() {
   console.log('Spell-checking the remaining files...');
   const misspellingsByFile = await Promise.all(_.map(treeEntries, async entry => {
     const blob = await entry.getBlob();
-    const misspellings = await getMisspellings(blob.toString());
+    const misspellings = await getMisspellings(blob.toString().replace(/\r\n/g, '\n'));
     return _.map(misspellings, misspelling => _.assign({}, misspelling, {
       path: entry.path(),
     }));
