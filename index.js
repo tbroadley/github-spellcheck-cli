11d2b99f71bdb1fcdfd0051e766c246468e99db6
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-13 21:56:53 -0500

    Allow periods in repo names

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ let clonePath;
 
 function parseRepo(repo) {
   const regexes = [
-    /^(?:https?:\/\/)?(?:www\.)?github\.com\/([-\w]+)\/([-_\w]+)$/,
-    /^([-\w]+)\/([-_\w]+)$/,
+    /^(?:https?:\/\/)?(?:www\.)?github\.com\/([-\w]+)\/([-_\w\.]+)$/,
+    /^([-\w]+)\/([-_\w\.]+)$/,
   ];
   const matchingRegex = _.find(regexes, re => re.test(repo));
   if (matchingRegex) {
