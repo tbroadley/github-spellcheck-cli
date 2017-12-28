3d468df54a012229c1146427ce2580772098c4a5
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-28 09:05:16 -0500

    Print usage when --help is passed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage');
 const glob = require('globby');
 const sum = require('lodash/sum');
 const report = require('vfile-reporter');
@@ -6,23 +7,50 @@ const report = require('vfile-reporter');
 const { Spellchecker } = require('./lib/spellchecker');
 
 (async () => {
-  const optionDefinitions = [
+  const optionList = [
     {
       name: 'files',
       alias: 'f',
+      typeLabel: '<file|glob> <file|glob>...',
+      description: 'A list of files or globs to spellcheck.',
       multiple: true,
       defaultOption: true,
     },
     {
       name: 'dictionary',
       alias: 'd',
+      typeLabel: '<file>',
+      description: 'A file to use as a personal dictionary.',
+    },
+    {
+      name: 'help',
+      alias: 'h',
+      type: Boolean,
+      description: 'Print this help screen.',
     },
   ];
 
   const {
     files,
     dictionary: personalDictionaryPath,
-  } = commandLineArgs(optionDefinitions);
+    help,
+  } = commandLineArgs(optionList);
+
+  const usage = getUsage([
+    {
+      header: 'spellcheck',
+      content: 'A command-line tool for spellchecking files.',
+    },
+    {
+      header: 'Options',
+      optionList,
+    },
+  ]);
+
+  if (help) {
+    console.log(usage);
+    return;
+  }
 
   const spellchecker = new Spellchecker();
   await spellchecker.init(personalDictionaryPath);
