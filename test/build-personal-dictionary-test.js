3685dcf5b19466f540626fcc330239bfcf1fa3ef
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-04-24 14:06:47 -0400

    Add buildPersonalDictionary tests

---
+++ test/build-personal-dictionary-test.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+
+const { buildPersonalDictionary } = require('../lib/build-personal-dictionary');
+
+chai.should();
+
+describe('buildPersonalDictionary', () => {
+  it('returns an empty array when no dictionaries are specified', async () => {
+    (await buildPersonalDictionary([])).should.deep.equal([]);
+  });
+
+  it('reads a plaintext dictionary', async () => {
+    (await buildPersonalDictionary(['test/fixtures/dictionaries/one.txt'])).should.deep.equal([
+      /^Thisisnotaword$/,
+      /^preprocessed$/,
+    ]);
+  });
+
+  it('reads a plaintext dictionary that uses regex notation', async () => {
+    (await buildPersonalDictionary(['test/fixtures/dictionaries/regex.txt'])).should.deep.equal([
+      /^Thisisnot.*$/,
+      /^(pre)?processed$/,
+    ]);
+  });
+
+  it('reads a JavaScript dictionary', async () => {
+    (await buildPersonalDictionary(['test/fixtures/dictionaries/regex.js'])).should.deep.equal([
+      /^Thisisnotaword$/,
+      /^(pre)?processed$/,
+    ]);
+  });
+
+  it('reads multiple dictionaries', async () => {
+    (await buildPersonalDictionary([
+      'test/fixtures/dictionaries/one.txt',
+      'test/fixtures/dictionaries/regex.js',
+    ])).should.deep.equal([
+      /^Thisisnotaword$/,
+      /^preprocessed$/,
+      /^Thisisnotaword$/,
+      /^(pre)?processed$/,
+    ]);
+  });
+});
